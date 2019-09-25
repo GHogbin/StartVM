1d0eb3b3758344ac3015d80f43b168e5b9fd279c
GHogbin <graham.hogbin@microsoft.com>
2019-09-25 08:15:59 +0100

    Add files via upload

---
+++ StartVM.ps1
@@ -0,0 +1,18 @@
+﻿# Login into Azure
+$Credential = Get-Credential
+Connect-AzAccount -Credential $Credential 
+# List Subscriptions
+$Subscription = Get-AzSubscription | select Name, ID | Out-GridView -PassThru
+    Set-AzContext -SubscriptionID $($Subscription.Id)
+# List Resource Groups
+$resourcegroupname = Get-AzResourceGroup | Out-GridView -PassThru
+# List VM's in Resource Group
+$vmname = Get-AzVM -ResourceGroupName $($resourcegroupname.ResourceGroupName) | Out-GridView -PassThru
+# Start Selected VM
+Start-AzVM -Name $($vmname.Name) -ResourceGroupName $($resourcegroupname.ResourceGroupName)
+# Obtain Private IP  for VM
+$vmNIC = (Get-AzNetworkInterface -ResourceGroupName RG-Hopkind-ADM).Name
+$interface = Get-AzNetworkInterface -Name $vmNIC -ResourceGroupName RG-Hopkind-ADM
+$privateip = (Get-AzNetworkInterfaceIpConfig -NetworkInterface $interface).PrivateIpAddress
+# RDP into VM
+mstsc /v:$privateip
